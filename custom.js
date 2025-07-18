8869432075284e69b3a09c99089cb5bd404334d2
betredicustomjs <betredi180@outlook.com>
2025-07-18 13:47:51 +0300

    betredi124

--- custom.js
+++ custom.js
@@ -1882,6 +1882,23 @@
       // $("#game-chooser").before(miniGamesSection);
     }
 
+    function injectVipImageLoop() {
+      const interval = setInterval(() => {
+        const vipDiv = document.querySelector(".vip");
+
+        if (
+          window.location.pathname === "/tr/vip/" &&
+          vipDiv &&
+          !document.querySelector("#vip-image-injected")
+        )
+          alert(true);
+        {
+          vipDiv.innerHTML = `<img id="vip-image-injected" src="https://betredicustomjs.github.io/betrediofficial/images/vip/vip.png" style="width: 100%">`;
+          clearInterval(interval);
+        }
+      }, 300);
+    }
+
     function initCustomMiniGamesSlider() {
       const swiperEl = document.querySelector(
         "#mini-games-wrapper .myMiniGamesSwiper"
@@ -2307,6 +2324,8 @@
 
       slot_games = getSlotGames();
       casino_games = getCasinoGames();
+
+      injectVipImageLoop();
 
       if ($(".form__btn span").text().trim() === "Send Request") {
         $(".form__btn span").text("Talep Gönder");
@@ -3954,7 +3973,7 @@ ${
     />
     <a href="https://betredi124.com/${language}/promotion/1000000tl-cekim-imkani" id="payment-details-btn" style="display flex !important; align-items: center !important; justify-content: center !important; position: absolute; bottom: 10%; right: 7%; background: #630306; padding: 8px 10px; border-radius: 6px; z-index: 10; gap: 2px;">
       <span style="font-family: 'Afacad', sans-serif !important; letter-spacing: 3px;">DETAYLAR</span>
-      <svg width="26" height="47" viewBox="0 0 26 47" fill="none" xmlns="http://www.w3.org/2000/svg" style="width: 14px; height: 14px;">
+      <svg width="26" height="47" viewBox="0 0 26 47" fill="none" xmlns="http://www.w3.org/2000/svg" style="width: 14px; height: 14px; margin-top: -3px;">
 <path d="M2.51245 0.376953C2.11772 0.376953 1.71377 0.533008 1.41084 0.835938C0.804979 1.4418 0.804979 2.4332 1.41084 3.03906L22.1478 23.776L1.71377 44.21C1.10791 44.8158 1.10791 45.8072 1.71377 46.4131C2.31963 47.0189 3.31104 47.0189 3.91689 46.4131L25.4616 24.8775C26.0675 24.2717 26.0675 23.2803 25.4616 22.6744L3.62315 0.835938C3.31104 0.523828 2.91635 0.376953 2.51245 0.376953Z" fill="url(#paint0_linear_1237_459)"/>
 <defs>
 <linearGradient id="paint0_linear_1237_459" x1="13.4362" y1="0.376953" x2="13.4362" y2="46.8675" gradientUnits="userSpaceOnUse">
